aa8acf2cc6969c628795eed49eb2a1e516bc2282
wenjing-liu <wenjing.liu09@foxmail.com>
2018-05-14 18:52:01 +0800

    recipe controller

--- src/js/index.js
+++ src/js/index.js
@@ -27,13 +27,18 @@ const controlSearch = async () => {
         searchView.clearInput()
         searchView.clearResults()
         renderLoader(elements.searchRes)
-
+        
+        try {
         // 4) Search for recipes
         await state.search.getResults()
 
         // 5) Rnder results on UI
         clearLoader()
         searchView.renderResults(state.search.result)
+        } catch (error) {
+            alert('Something went wrong:)')
+            clearLoader()
+        }
     }
 }
 
@@ -55,7 +60,29 @@ elements.searchResPages.addEventListener('click', e => {
 /**
  * RECIPE CONTROLLER
  */
+const controlRecipe = async () => {
+    // Get ID from url
+    const id = window.location.hash.replace('#', '')
+    console.log(id)
+    if (id) {
+        // Prepare UI for changes
 
- const r = new Recipe(46956)
- r.getRecipe()
- console.log(r)
+        // Create new recipe object
+        state.recipe = new Recipe(id)
+        
+        try {
+            // Get recipe data
+            await state.recipe.getRecipe()
+            // Caculate servings and time
+            state.recipe.calcTime()
+            state.recipe.calcServings()
+            // Render recipe
+            console.log(state.recipe)
+        } catch(err) {
+            alert('Error processing recipe')
+        }
+        
+    }
+}
+
+ ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe))
