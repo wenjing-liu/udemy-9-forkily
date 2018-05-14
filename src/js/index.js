b6556ef53a0a117d77674efe877d27151652a909
wenjing-liu <wenjing.liu09@foxmail.com>
2018-05-14 13:10:21 +0800

    babel config

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 // Global app controller
 import num from './test'
+const x = 23
 
-
-console.log(`I imported ${num} from another module called test`)
+console.log(`I imported ${num} from another module called test.js! Variable x is ${x}`)
